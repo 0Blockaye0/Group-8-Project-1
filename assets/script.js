48adc5b6cae93ea8a9a673d6e1608746587e2c76
_blockaye_ <blake_austinjohnson@icloud.com>
2021-03-28 08:06:21 -0500

    weather api and lastfm api working together

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,30 @@
 var userSearch = document.getElementById("search-container");
 
-var getArtist = function(artist) {
+var getTracks = function (weatherSearchTerm) {
+  var apiKey = "14101bf418a50454455bae74560f1204";
 
-    var apiKey = "14101bf418a50454455bae74560f1204"
-    
-    var apiUrl = `http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag=${artist}&api_key=${apiKey}&format=json`
-    // var apiUrl = `http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artist}&api_key=${apiKey}&format=json`;
-    
-    // make a request to the url
-    fetch(apiUrl)
-    .then(function(response) {
+  //var apiUrl = `http://ws.audioscrobbler.com/2.0/?method=tag.getSimilar&tag=${artist}&api_key=${apiKey}&format=json`
+  var apiUrl = `http://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=${weatherSearchTerm}&api_key=${apiKey}&format=json`;
+  // var apiUrl = `http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag=${artist}&api_key=${apiKey}&format=json`
+  // var apiUrl = `http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artist}&api_key=${apiKey}&format=json`;
+
+  // make a request to the url
+  fetch(apiUrl)
+    .then(function (response) {
       // request was successful
       if (response.ok) {
-        response.json().then(function(data) {
-            console.log(data);
+        response.json().then(function (data) {
+          console.log(data);
           // function(data);
         });
       } else {
         alert("Error: " + response.statusText);
       }
     })
-    .catch(function(error) {
-      alert("Unable to connect to the API");
+    .catch(function (error) {
+      alert("Unable to connect to the lastfm API");
     });
-    
 };
-
 
 var searchHandler = function (event) {
   event.preventDefault();
@@ -38,55 +37,39 @@ var searchHandler = function (event) {
   console.log(userInput);
 
   if (userInput) {
-    getArtist(userInput);
+    getWeather(userInput);
     userSearchInputEl.value = "";
   } else {
-    alert("please enter an Artist");
+    alert("please enter a city and state. Ex: 'Austin, TX'.");
   }
 };
 
-
-//getArtist("Jerry Garcia");
-
-
-
-
 userSearch.addEventListener("submit", searchHandler);
 
+var getWeather = function(cityName, stateCode) {
+  var apiKey = "d0f4ff36139913f4de0b35a854f44600";
+  var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName},${stateCode}&appid=${apiKey}`;
 
+  // make a request to the url
+  fetch(apiUrl)
+  .then(function(response) {
+    // request was successful
+    if (response.ok) {
+      response.json().then(function(data) {
+          console.log("this is the weather data: ", data);
+          var weatherSearchTerm = data.weather[0].main;
+          console.log(weatherSearchTerm);
 
+          getTracks(weatherSearchTerm);
+      });
+    } else {
+      alert("Error: " + response.statusText);
+    }
+  })
+  .catch(function(error) {
+    alert("Unable to connect to the weatherAPI");
+  });
 
+};
 
-
-
-
-
-
-// var getThis = function(variable) {
-//   var apiKey = "9bfa8210942fcc00671aa6f4917ce745ee5f469d";
-    
-//   //var apiUrl = `http://api.music-story.com/en/release/search?release_date=${variable}&apiKey=${apiKey}`;
-
-//   var getTokensApiUrl = `http://api.music-story.com/oauth/request_token?oauth_consumer_key=${apiKey}&oauth_signature=GET&http://api.music-story.com/oauth/request_token&<ENCODED PARAMETERS>`
-  
-//   // make a request to the url
-//   fetch(apiUrl)
-//   .then(function(response) {
-//     // request was successful
-//     if (response.ok) {
-//       response.json().then(function(data) {
-//           console.log(data);
-//         // function(data);
-//       });
-//     } else {
-//       alert("Error: " + response.statusText);
-//     }
-//   })
-//   .catch(function(error) {
-//     alert("Unable to connect to the API");
-//   });
-  
-// };
-
-// getThis("02/14/1990");
-
+//getWeather("Austin, TX");
