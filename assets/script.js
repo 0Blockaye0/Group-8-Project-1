bf1178eff689c590d0db23b58964e076736580a1
_blockaye_ <blake_austinjohnson@icloud.com>
2021-03-29 10:01:43 -0500

    code is cleaned up and secondary array for tags is working

--- assets/script.js
+++ assets/script.js
@@ -2,34 +2,86 @@ var userSearch = document.getElementById("search-container");
 
 var trackArray = [];
 
-likeCloudsArr = ["chill", "lazy", "gloom", "vapour", "haze", "fog", "daze", "sad"];
-likeRainArr = ["wet", "damp", "pouring", "thunder", "storm", "peacful", "indoors", "flood", "lightning"];
-likeClearArr = ["air", "clearly", "open"];
-likeSunnyArr = ["bright", "sunshine","warm", "flowers", "blue sky", "hot", "heat", "beach", "summer"];
-likeSnowArr = ["cold", "winter", "peacfull", "ice", "blizzard", "snowy", ];
+likeCloudsArr = [
+  "chill",
+  "lazy",
+  "gloom",
+  "vapour",
+  "haze",
+  "fog",
+  "daze",
+  "sad",
+];
 
+likeRainArr = [
+  "wet",
+  "damp",
+  "pouring",
+  "thunder",
+  "storm",
+  "peacful",
+  "indoors",
+  "flood",
+  "lightning",
+];
 
+likeClearArr = [
+"air", 
+"fresh", 
+"open"
+];
 
+likeSunnyArr = [
+  "bright",
+  "sunshine",
+  "warm",
+  "flowers",
+  "blue sky",
+  "hot",
+  "heat",
+  "beach",
+  "summer",
+];
 
-//////////////////////////////////////
-/// currently only accepting "Clouds" and "Clear" as a condition to get more tracks
-var getSimilarTags = function(tag) {
+likeSnowArr = [
+  "cold", 
+  "winter", 
+  "peacfull", 
+  "ice", 
+  "blizzard", 
+  "snowy"
+];
+
+likeDrizzleArr = [
+  "beats", 
+  "chill", 
+  "cool", 
+  "vibe"
+];
+
+var getSimilarTags = function (tag) {
   if (tag === "Clouds") {
-      newTag = likeCloudsArr[Math.floor(Math.random() * likeCloudsArr.length)];
-      console.log(newTag);
-      getMoreTracks(newTag);
-  } else {
-    if (tag === "Clear") {
-      newTag = likeClearArr[Math.floor(Math.random() * likeClearArr.length)];
-      console.log(newTag);
-      getMoreTracks(newTag);
-    };
-  };
+    newTag = likeCloudsArr[Math.floor(Math.random() * likeCloudsArr.length)];
+    console.log(newTag);
+    getMoreTracks(newTag);
+  } else if (tag === "Clear") {
+    newTag = likeClearArr[Math.floor(Math.random() * likeClearArr.length)];
+    console.log(newTag);
+    getMoreTracks(newTag);
+  } else if (tag === "Rain") {
+    newTag = likeRainArr[Math.floor(Math.random() * likeRainArr.length)];
+    console.log(newTag);
+    getMoreTracks(newTag);
+  } else if (tag === "Sunny") {
+    newTag = likeSunnyArr[Math.floor(Math.random() * likeSunnyArr.length)];
+    console.log(newTag);
+    getMoreTracks(newTag);
+  } else if (tag === "Snow") {
+    newTag = likeSnowArr[Math.floor(Math.random() * likeSnowArr.length)];
+    console.log(newTag);
+    getMoreTracks(newTag);
+  }
 };
-//////////////////////////////////////
-
-
-
 
 var getMoreTracks = function (newTag) {
   //console.log(newTag);
@@ -49,7 +101,6 @@ var getMoreTracks = function (newTag) {
           // console.log(data);
           trackArray.push(data.tracks.track);
           console.log(trackArray);
-          
         });
       } else {
         alert("Error: " + response.statusText);
@@ -75,11 +126,11 @@ var getTracks = function (weatherSearchTerm) {
       if (response.ok) {
         response.json().then(function (data) {
           // console.log(data.tracks.track[0]);
+          trackArray = [];
           trackArray.push(data.tracks.track);
           //console.log(trackArray);
           //console.log(weatherSearchTerm);
           getSimilarTags(weatherSearchTerm);
-        
         });
       } else {
         alert("Error: " + response.statusText);
@@ -108,37 +159,34 @@ var searchHandler = function (event) {
   }
 };
 
-userSearch.addEventListener("submit", searchHandler);
-
-var getWeather = function(cityName, stateCode) {
+var getWeather = function (cityName, stateCode) {
   var apiKey = "d0f4ff36139913f4de0b35a854f44600";
   var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName},${stateCode}&appid=${apiKey}`;
 
   // make a request to the url
   fetch(apiUrl)
-  .then(function(response) {
-    // request was successful
-    if (response.ok) {
-      response.json().then(function(data) {
+    .then(function (response) {
+      // request was successful
+      if (response.ok) {
+        response.json().then(function (data) {
           console.log("this is the weather data: ", data);
           var weatherSearchTerm = data.weather[0].main;
           console.log(weatherSearchTerm);
 
           getTracks(weatherSearchTerm);
           return weatherSearchTerm;
-      });
-    } else {
-      alert("Error: " + response.statusText);
-    }
-  })
-  .catch(function(error) {
-    alert("Unable to connect to the weatherAPI");
-  });
-
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert("Unable to connect to the weatherAPI");
+    });
 };
 
-var displayTracks = function (trackArray) {
+var displayTracks = function (trackArray) {};
 
-};
+userSearch.addEventListener("submit", searchHandler);
 
 //getWeather("Austin, TX");
